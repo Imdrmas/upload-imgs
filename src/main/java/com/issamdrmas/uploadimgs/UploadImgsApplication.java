d2a9a2255a1881e92d7213a9fa025678ca16602c
ISSAM DRMAS <issamdrmas@ISSAMs-MacBook-Pro.local>
2020-08-19 11:36:34 +0200

    first

---
+++ src/main/java/com/issamdrmas/uploadimgs/UploadImgsApplication.java
@@ -0,0 +1,13 @@
+package com.issamdrmas.uploadimgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadImgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadImgsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/issamdrmas/uploadimgs/UploadImgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.issamdrmas.uploadimgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadImgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
